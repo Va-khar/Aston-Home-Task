551e0e55ee56beff152bcd7c8985e00e4359804d
Arkhip Vakhar <81294023+Va-khar@users.noreply.github.com>
2024-08-07 12:15:28 +0300

    Выполнение

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,4 @@
 
 public class Main {
-    public static void main(String[] args) {
-        String f1 = "Orange";
-        String f2 = "Banana";
-        String f3 = "Apple";
-        System.out.println();
-
-    }
+    public static void main(String[] args) {}
 }
